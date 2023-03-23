4a78268b698408ee6ef34444aa6302a411c10f34
Corentin <corentin@seventeeninfo.fr>
2023-03-23 10:29:18 +0100

    push du jeudi 23/03/2023 à 10:29AM

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+var data = [
+    {
+      "category": "Reaction",
+      "score": 80,
+      "icon": "./assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "icon": "./assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "icon": "./assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "icon": "./assets/images/icon-visual.svg"
+    }
+  ];  
+
+data.forEach((result)=>{
+    if(document.querySelector('[data-category="'+result.category+'"]'))
+        document.querySelector('[data-category="'+result.category+'"]').querySelector('div:last-child > p > b').textContent = result.score;
+});
